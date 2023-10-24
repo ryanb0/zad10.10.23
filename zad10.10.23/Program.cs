66cd8b5f3c263e4c460434ce6bd86ff35036155f
Ryan <Ryan@ATLAS-1LMUN5FIG>
2023-10-24 10:50:31 +0200

    Add project files.

---
+++ zad10.10.23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace a
+{
+    class Produkt
+    {
+        string nazwaProducenta;
+        string nazwaProduktu;
+        int kategoria;
+        string kodproduktu;
+        double cena;
+        string opis;
+    }
+    /*
+    class Magazyn
+    {
+        produkty;
+        adresMagazynu;
+    }
+    class adres
+    {
+        ulica;
+        kodpocztowy;
+        miejscowosc;
+        numerPosesji;
+        numerLokalu;
+    }
+    */
+}
